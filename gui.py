62efd0f232c9d440d88522153c36af91e3a5cefa
Benji <50681275+Benji377@users.noreply.github.com>
2022-04-18 18:51:32 +0200

    Added colors to the GUI

--- gui.py
+++ gui.py
@@ -25,30 +25,34 @@ titel = Label(frame, text="WebScraper GUI", font=title_font, fg="red", bg=frame_
 titel.pack(side=TOP, pady=5)
 
 # Frame that contains the Label specifying where to insert the URL and the Entry
-url_frame = Frame(frame)
-url_label = Label(url_frame, text="URL:", font=normal_text)
+url_frame = Frame(frame, bg=frame_background)
+url_label = Label(url_frame, text="URL:", font=normal_text, bg=frame_background)
 url_label.pack(side=LEFT, padx=25)
 url_input = Entry(url_frame)
 url_input.pack(fill=X, padx=20)
 url_frame.pack(fill=X, pady=20)
 
 # Same as the url_frame
-dir_frame = Frame(frame)
-dir_label = Label(dir_frame, text="Directory:", font=normal_text)
+dir_frame = Frame(frame, bg=frame_background)
+dir_label = Label(dir_frame, text="Directory:", font=normal_text, bg=frame_background)
 dir_label.pack(side=LEFT, padx=8)
 dir_input = Entry(dir_frame)
 dir_input.pack(fill=X, padx=20)
 dir_frame.pack(fill=X, pady=20)
 
 # A frame that contains all the checkboxes
-check_frame = Frame(frame)
-html_check = Checkbutton(check_frame, text="HTML", variable=html_bool, onvalue=True, offvalue=False, font=normal_text)
+check_frame = Frame(frame, bg=frame_background)
+html_check = Checkbutton(check_frame, text="HTML", variable=html_bool, onvalue=True, offvalue=False, font=normal_text,
+                         bg=frame_background)
 html_check.pack(side=LEFT, padx=20)
-text_check = Checkbutton(check_frame, text="Text", variable=text_bool, onvalue=True, offvalue=False, font=normal_text)
+text_check = Checkbutton(check_frame, text="Text", variable=text_bool, onvalue=True, offvalue=False, font=normal_text,
+                         bg=frame_background)
 text_check.pack(side=LEFT, padx=20)
-links_check = Checkbutton(check_frame, text="Links", variable=links_bool, onvalue=True, offvalue=False, font=normal_text)
+links_check = Checkbutton(check_frame, text="Links", variable=links_bool, onvalue=True, offvalue=False,
+                          font=normal_text, bg=frame_background)
 links_check.pack(side=LEFT, padx=20)
-images_check = Checkbutton(check_frame, text="Images", variable=images_bool, onvalue=True, offvalue=False, font=normal_text)
+images_check = Checkbutton(check_frame, text="Images", variable=images_bool, onvalue=True, offvalue=False,
+                           font=normal_text, bg=frame_background)
 images_check.pack(side=LEFT, padx=20)
 check_frame.pack(fill=X, pady=20)
 
