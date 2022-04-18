e79b55f7ebcf228907ae5a7f267b3562a4868d9e
Benji <50681275+Benji377@users.noreply.github.com>
2022-04-18 18:27:16 +0200

    Added first GUI prototype

--- gui.py
+++ gui.py
@@ -2,9 +2,75 @@
 # TODO: This is a work in progress
 
 from tkinter import *
+from tkinter import messagebox
 
 root = Tk()
-a = Label(root, text="Hello World")
-a.pack()
+root.geometry("400x300")
+frame = Frame(root)
+frame.pack(fill=BOTH, expand=TRUE)
 
+html_bool = BooleanVar()
+text_bool = BooleanVar()
+links_bool = BooleanVar()
+images_bool = BooleanVar()
+
+titel = Label(frame, text="WebScraper GUI")
+titel.pack(side=TOP, pady=5)
+
+url_frame = Frame(frame)
+url_label = Label(url_frame, text="URL:")
+url_label.pack(side=LEFT, padx=22)
+url_input = Entry(url_frame)
+url_input.pack(fill=X, padx=20)
+url_frame.pack(fill=X, pady=20)
+
+dir_frame = Frame(frame)
+dir_label = Label(dir_frame, text="Directory:")
+dir_label.pack(side=LEFT, padx=8)
+dir_input = Entry(dir_frame)
+dir_input.pack(fill=X, padx=20)
+dir_frame.pack(fill=X, pady=20)
+
+check_frame = Frame(frame)
+html_check = Checkbutton(check_frame, text="HTML", variable=html_bool, onvalue=True, offvalue=False)
+html_check.pack(side=LEFT, padx=20)
+text_check = Checkbutton(check_frame, text="Text", variable=text_bool, onvalue=True, offvalue=False)
+text_check.pack(side=LEFT, padx=20)
+links_check = Checkbutton(check_frame, text="Links", variable=links_bool, onvalue=True, offvalue=False)
+links_check.pack(side=RIGHT, padx=20)
+images_check = Checkbutton(check_frame, text="Images", variable=images_bool, onvalue=True, offvalue=False)
+images_check.pack(side=RIGHT, padx=20)
+check_frame.pack(fill=X, pady=20)
+
+
+def retrieve_data():
+    from scraper import Scraper
+    url = str(url_input.get())
+    direct = str(dir_input.get())
+
+    try:
+        scrap = Scraper(url, direct)
+        print(scrap.response)
+        if not html_bool.get() and not text_bool.get() and not links_bool.get() and not images_bool.get():
+            messagebox.showinfo("Nothing selected", "You didn't select any option")
+        else:
+            if html_bool.get():
+                scrap.get_html()
+            if text_bool.get():
+                scrap.get_text()
+            if links_bool.get():
+                scrap.get_links()
+            if images_bool.get():
+                scrap.get_images()
+            messagebox.showinfo("Done", "Website has been scraped")
+    except Exception as err:
+        print(err)
+        messagebox.showerror("Error", str(err))
+
+
+submit_button = Button(frame, text="Submit", command=retrieve_data)
+submit_button.pack(side=BOTTOM, pady=30)
+
+
+root.title("WebScraper Gui")
 root.mainloop()
--- scraper.py
+++ scraper.py
@@ -11,19 +11,18 @@ class Scraper:
     # Response of the GET-request
     response = None
     # Dictionary to save the files to
-    dir_path = None
+    dir_path = ""
 
     # Class constructor requires the URL to the website and an existing directory
     def __init__(self, url, dirp):
         # check status code for response received
         # success code - 200
         if self.make_request(url) and self.check_dir(dirp):
-            print(self.dir_path)
-            print(self.response)
             # Parsing the HTML
             self.soup = BeautifulSoup(self.response.content, 'html.parser')
+            print("Scraper initialized - Soup created")
         else:
-            print("Failed")
+            print("Scraper initialization failed")
             raise Exception("Invalid URL or Path")
 
     def make_request(self, link):
@@ -32,25 +31,37 @@ class Scraper:
         if validators.url(link):
             # If the String is a link, it makes the GET-request
             self.response = requests.get(link)
+            print("Response code: " + str(self.response))
             ret = True
+        else:
+            print("Invalid URL")
         return ret
 
     # Check if the given path is a valid directory
     def check_dir(self, path):
         ret = False
         # Checks if the dir exists and is valid or if it is empty
-        if os.path.isdir(path) or path == "":
-            self.dir_path = path
+        if not path:
+            self.dir_path = os.getcwd() + "\\"
+            print("No path selected, using current dir:\n"
+                  + self.dir_path)
             ret = True
+        elif os.path.isdir(path):
+            self.dir_path = path
+            print("Path specified: " + self.dir_path)
+            ret = True
+        else:
+            print("Unclear path error, path might be invalid or not exist")
         return ret
 
     # Returns the whole website with HTML tags
     def get_html(self):
         html_file_path = self.dir_path + "website.html"
         # Write the retrieved contents to a newly created file
-        f = open(html_file_path, "w")
+        f = open(html_file_path, "w", encoding="utf-8")
         f.write(self.soup.prettify())
         f.close()
+        print("Wrote HTML contents to: " + html_file_path)
 
     # Returns only the text inside the <p> tag
     def get_text(self):
@@ -62,9 +73,10 @@ class Scraper:
             ret += str(line.text) + "\n"
 
         # Write string to file
-        f = open(text_file_path, "w")
+        f = open(text_file_path, "w", encoding="utf-8")
         f.write(ret)
         f.close()
+        print("Wrote TXT contents to: " + text_file_path)
 
     # Returns all the links inside an <a> tag
     def get_links(self):
@@ -75,9 +87,10 @@ class Scraper:
         for link in self.soup.find_all('a'):
             ret += str(link.get('href')) + "\n"
 
-        f = open(links_file_path, "w")
+        f = open(links_file_path, "w", encoding="utf-8")
         f.write(ret)
         f.close()
+        print("Wrote LINKS contents to: " + links_file_path)
 
     # Returns all the images inside an <img> tag
     def get_images(self):
@@ -95,10 +108,7 @@ class Scraper:
         for image in images_list:
             ret += str(image) + "\n"
 
-        f = open(images_file_path, "w")
+        f = open(images_file_path, "w", encoding="utf-8")
         f.write(ret)
         f.close()
-
-
-scrap = Scraper('https://www.geeksforgeeks.org/python-programming-language/', "")
-scrap.get_links()
+        print("Wrote IMAGE contents to: " + images_file_path)
